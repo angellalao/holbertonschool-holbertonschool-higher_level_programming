3130df80eed96a39893c1f3b8f776a451376d7b5
Angella Lao <6369@holbertonstudents.com>
2023-09-11 14:16:39 +1000

    replace var with let

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -2,6 +2,6 @@ const red_header = document.querySelector("#red_header");
 red_header.onclick = addClassRed;
 
 function addClassRed() {
-    var element = document.getElementsByTagName("header")[0];
+    let element = document.getElementsByTagName("header")[0];
     element.classList.add("red")
 }
--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -2,7 +2,7 @@ const toggle_header = document.querySelector("#toggle_header");
 toggle_header.onclick = switchClassRedGreen;
 
 function switchClassRedGreen() {
-    var element = document.getElementsByTagName("header")[0];
+    let element = document.getElementsByTagName("header")[0];
     element.classList.toggle("red")
     element.classList.toggle("green")
 }
